f69d78ebc5168020115ed13fa1a8ba71f48c8663
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2020-06-13 11:56:47 +0900

    modify: App.vue => store/modules/counter 動作確認用改修

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">
+        Home
+      </router-link>
+      |
+      <router-link to="/about">
+        About
+      </router-link>
     </div>
     <router-view />
+    <button @click="inc">
+      inc
+    </button>
+    <button @click="dcr">
+      dcr
+    </button>
+    <div>{{ count }} ^ 2 = {{ exponentiationCount }}</div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { CounterModule } from '@/store/modules/counter'
+
+@Component
+export default class App extends Vue {
+  private inc(): void {
+    CounterModule.increment()
+  }
+  private dcr(): void {
+    console.log('dec')
+    CounterModule.decrement()
+  }
+
+  private get count(): number {
+    return CounterModule.count
+  }
+
+  private get exponentiationCount(): number {
+    return CounterModule.exponentiationCount
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
