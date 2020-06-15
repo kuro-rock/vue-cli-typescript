3294f44607aceffaa677a42b9401f79eaf0a7bb5
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2020-06-16 08:43:12 +0900

    modify: App.vue => カウンター表示を別Viewsに分離

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,41 +8,14 @@
       <router-link to="/about">
         About
       </router-link>
+      |
+      <router-link to="/counter">
+        Counter
+      </router-link>
     </div>
     <router-view />
-    <button @click="inc">
-      inc
-    </button>
-    <button @click="dcr">
-      dcr
-    </button>
-    <div>{{ count }} ^ 2 = {{ exponentiationCount }}</div>
   </div>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import { CounterModule } from '@/store/modules/counter'
-
-@Component
-export default class App extends Vue {
-  private inc(): void {
-    CounterModule.increment()
-  }
-  private dcr(): void {
-    console.log('dec')
-    CounterModule.decrement()
-  }
-
-  private get count(): number {
-    return CounterModule.count
-  }
-
-  private get exponentiationCount(): number {
-    return CounterModule.exponentiationCount
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
